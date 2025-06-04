131a6b4ac668ecb13200d24d56db16e71ce8df90
Kishan <kishanshukla53@gmail.com>
2025-06-04 22:08:07 +0530

    init

--- src/reAct_Arcitecture.ts
+++ src/reAct_Arcitecture.ts
@@ -1,25 +1,40 @@
-// model init
-// bind tool
-// create a workflow
-// invoke
-
 import { BaseMessage } from "@langchain/core/messages";
 import { tool } from "@langchain/core/tools";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { Annotation } from "@langchain/langgraph";
+import dotenv from "dotenv";
+dotenv.config();
 
+
+// creating state
 const AgentState = Annotation.Root({
     message : Annotation<Array<BaseMessage>>
 })
 
-const add = tool(async()=>{},{
+
+// defining tool
+const add = tool(async(query)=>{
+  console.log(query)
+},{
     name:"Addition Tool",
     description:"when given 2 numbers add them"
 })
 
+
+// tool 
 const tools = [add]
 
+
+// model init
 const llm = new ChatGoogleGenerativeAI({
   model: "gemini-2.0-flash",
-  temperature: 0
+  temperature: 0,
+  apiKey:process.env.GOOGLE_API_KEY
 }).bindTools(tools)
+
+
+
+
+const result = async () => {
+    
+}
